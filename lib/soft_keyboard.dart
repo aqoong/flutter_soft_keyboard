469fb2cb700bc5fb690170e97b33b23aa087b51f
wonwoohan <cooldnjsdn@gmail.com>
2025-02-09 08:49:12 +0900

    add ripple container and key type
    Adds a ripple container for visual feedback and a key type enum to the soft keyboard package.

--- lib/soft_keyboard.dart
+++ lib/soft_keyboard.dart
@@ -1,6 +1,11 @@
 library flutter_soft_keyboard;
 
 export 'key/virtual_key.dart';
+export 'key/key_type.dart';
 export 'key/key_widget.dart';
 export 'keyboard_input_controller.dart';
 export 'soft_keyboard_widget.dart';
+
+/// [aqoong/ripple_container]
+/// https://pub.dev/packages/ripple_container
+export 'package:ripple_container/ripple_container.dart';
